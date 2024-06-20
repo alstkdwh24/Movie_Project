bd7356cdc3e36cfba2a3334ccecf32259687f579
alstkdwh24 <alstkdwh24@naver.com>
2024-06-21 00:29:15 +0900

    모달적용성공

--- src/main/resources/static/js/modal/mains_menu_modal.js
+++ src/main/resources/static/js/modal/mains_menu_modal.js
@@ -1,19 +1,37 @@
-// document.addEventListener('DOMContentLoaded', (event) => {
-//     const mo = document.getElementById("modal_good");
-//     const bu = document.getElementById("community");
-//     const closeButtons = document.querySelectorAll(".close_closes");
-//
-//     console.log(mo.length);
-//     console.log(bu.length);
-//     console.log(closeButtons.length);
-//
-//     bu.addEventListener('click', () => {
-//         mo.classList.remove('modalss');
-//     });
-//
-//     closeButtons.forEach(closeButton => {
-//         closeButton.addEventListener('click', () => {
-//             mo.classList.add('modalss');
-//         });
-//     });
-// });
+const mo = document.querySelectorAll(".modal_good");
+const bu = document.querySelectorAll(".community");
+const closeButtons = document.querySelectorAll(".close_closes");
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+
+    console.log("Number of buttons:", bu.length);
+    console.log("Number of modals:", modals.length);
+    console.log("Number of close buttons:", closeButtons.length);
+
+    bu.forEach((button, index) => {
+        if (index < modals.length) {
+            button.onclick = function () {
+                mo[index].style.display = "flex";
+                mo[index].style.justifyContent = "center";
+                mo[index].style.alignItems = "center";
+            }
+        }
+    });
+
+    closeButtons.forEach((close, index) => {
+        if (index < modals.length) {
+            close.onclick = function () {
+                mo[index].style.display = "none";
+            }
+        }
+    });
+
+    window.onclick = function (event) {
+        mo.forEach(mo => {
+            if (event.target === mo) {
+                mo.style.display = "none";
+            }
+        });
+    };
+});
