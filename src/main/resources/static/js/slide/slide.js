00aa767fb1b4b44aeccf156428486cf021cc8556
alstkdwh24 <alstkdwh24@naver.com>
2024-06-21 22:59:32 +0900

    와 기분좋다

--- src/main/resources/static/js/slide/slide.js
+++ src/main/resources/static/js/slide/slide.js
@@ -7,8 +7,8 @@ document.addEventListener('DOMContentLoaded', (event) => {
         "img/K9uVM4dfQUtUEuZLwXhW1RDcKbA3z9saajgdSa4yXC9auIiGJUtKmKRlX5DBlsLzInndp8VLddsK1JCt1aiyNWAga_QEy6G_0l54TzAXD3nmYYznjMBPTgVQVESecjoNpB-qiSDFt-7qWYc8zEkBtA.webp",
         "img/4w84T0Vao2O_uvFZwuxehrec3fgBt0PBETJa2-5dxghEL8tWr6VjzMHpPjKLH89fV_UwjShjQRZlOwP03FnmsV2WLzGJ9Cv5YHmLjN6lwi8CBQjXIqO2cm7gzHW42q-QFXjmt2Rl3O6b2WKM2lNaSA.webp",
         "img/f793ec0c-d54d-46a6-822b-48b3902fda29.jpg",
-        "img/스즈매의 문단속.jpeg",
-        "img/너의 이름은.jpg"];
+        "img/door.jpeg",
+        "img/your_name.jpg"];
     let currentindex = 0;
     const bigImageElement = document.querySelector(".big_image");
     const big_image_arrow_button = document.querySelector(".big_image_arrow_button");
