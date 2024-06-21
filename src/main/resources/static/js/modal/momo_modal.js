e5acfaca2b90f5a6d79aea8389f2412287819fc9
alstkdwh24 <alstkdwh24@naver.com>
2024-06-21 22:41:29 +0900

    영화포스터 적용

--- src/main/resources/static/js/modal/momo_modal.js
+++ src/main/resources/static/js/modal/momo_modal.js
@@ -1,14 +1,15 @@
-const momo_modal=document.querySelectorAll(".momo_container");
 document.addEventListener('DOMContentLoaded',(event) =>{
-   const buttonmodal=document.querySelectorAll(".momo_button");
-   const letclose=document.querySelectorAll(".close_lo");
-   console.log(buttonmodal.length);
-   console.log(momo_modal.length);
-   console.log(letclose.length);
+   const button_modal=document.querySelectorAll(".momo_button");
+   const let_close=document.querySelectorAll(".close_lo");
+    const momo_modal=document.querySelectorAll(".momo_container");
 
-   buttonmodal.forEach((button,index)=>{
+    console.log(button_modal.length);
+   console.log(momo_modal.length);
+   console.log(let_close.length);
+
+   button_modal.forEach((button,index)=>{
        if(index<momo_modal.length){
-           buttonmodal.onclick = function (){
+           button.onclick = function (){
                momo_modal[index].style.display="flex";
                momo_modal[index].style.justifyContent='center';
                momo_modal[index].style.alignItems="center";
@@ -17,27 +18,27 @@ document.addEventListener('DOMContentLoaded',(event) =>{
 
 
        });
-    letclose.forEach((closese, index)=> {
+    let_close.forEach((close_se, index)=> {
     if(index<momo_modal.length){
-        closese.onclick=function(){
+        close_se.onclick=function(){
             momo_modal[index].style.display="none";
         }
     }
     });
-    momo_modal.forEach((element)=>{
-        element.addEventListener('click',(event)=>{
-            const modalContents=element.querySelectorAll(".mo_contents");
-            console.log(modalContents);
-            if(modalContents && modalContents.contains(event.target)){
-                event.stopPropagation();
-            }
-            }
-        );
-        element.addEventListener('click',(event)=>{
-            if(event.target===element){
-                element.style.display='none';
-            }
-        })
+ momo_modal.forEach((element)=>{
+    element.addEventListener('click',(event)=>{
+       const modalContents=element.querySelectorAll(".mo_contents");
+       console.log(modalContents,length);
+       if(modalContents && modalContents.contains(event.target)){
+           event.stopPropagation();
+       }
+
     });
+    element.addEventListener('click',(event)=>{
+       if(event.target===element){
+           element.style.display="none";
+       }
+    });
+ });
 
 });
--- src/main/resources/static/js/slide/slide.js
+++ src/main/resources/static/js/slide/slide.js
@@ -1,17 +1,37 @@
-let movie_menu=document.querySelectorAll(".movie_menu");
 
-document.addEventListener('DOMContentLoaded', (even) => {
-    let slideOpen = document.querySelectorAll(".image_button");
-    let slides = document.querySelectorAll(".slide_close");
-    slideOpen.forEach((slidesOpen,index)=>{
-    slidesOpen.onclick = function () {
-        movie_menu[index].style.left = 0;
-    }});
-    slides.forEach((sliders,index)=>{
-    sliders.onclick = function () {
-        movie_menu[index].style.left = "-100%";
-    }});
-    console.log(movie_menu.length);
-    console.log(slideOpen.length);
-    console.log(slides.length);
+document.addEventListener('DOMContentLoaded', (event) => {
+    const images = [
+        "img/330px-a.jpg",
+        "img/7gpCBgpVPqdCIqb_G7-d2Y8bXmGj5Pwlv5aPEPbHUwLZvR_xgaq64Re0DrhgcBkTwypFgQ6F99mnTzqcMB4nQVOrJ9v0S6zO8C9eGJ_msAxUKvopb84R0FPJmcH3iPk7bwDaBMWH-PDIZtcfPFt7Ug.webp",
+        "img/201930189_1280.jpg",
+        "img/K9uVM4dfQUtUEuZLwXhW1RDcKbA3z9saajgdSa4yXC9auIiGJUtKmKRlX5DBlsLzInndp8VLddsK1JCt1aiyNWAga_QEy6G_0l54TzAXD3nmYYznjMBPTgVQVESecjoNpB-qiSDFt-7qWYc8zEkBtA.webp",
+        "img/4w84T0Vao2O_uvFZwuxehrec3fgBt0PBETJa2-5dxghEL8tWr6VjzMHpPjKLH89fV_UwjShjQRZlOwP03FnmsV2WLzGJ9Cv5YHmLjN6lwi8CBQjXIqO2cm7gzHW42q-QFXjmt2Rl3O6b2WKM2lNaSA.webp",
+        "img/f793ec0c-d54d-46a6-822b-48b3902fda29.jpg",
+        "img/스즈매의 문단속.jpeg",
+        "img/너의 이름은.jpg"];
+    let currentindex = 0;
+    const bigImageElement = document.querySelector(".big_image");
+    const big_image_arrow_button = document.querySelector(".big_image_arrow_button");
+    const big_image_arrow_button_left = document.querySelector(".big_image_arrow_button_left");
+
+    function showSlide(index) {
+        bigImageElement.style.backgroundImage = `url(${images[index]})`;
+
+    }
+
+    function nextSlide() {
+        currentindex = (currentindex + 1) % images.length;
+        showSlide(currentindex);
+    }
+
+    function prevSlide() {
+        currentindex = (currentindex - 1 + images.length) % images.length;
+        showSlide(currentindex);
+    }
+
+    big_image_arrow_button.addEventListener('click', nextSlide);
+    big_image_arrow_button_left.addEventListener('click', prevSlide);
+
+
+    showSlide(currentindex);
 });
