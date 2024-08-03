448135736ea532ed8b840546ab6fda633769b3af
alstkdwh24 <alstkdwh24@naver.com>
2024-08-03 17:27:16 +0900

    아ㅏㄴ녕

--- src/main/resources/static/js/data/Use_Rest_js/Reservation_Rest.js
+++ src/main/resources/static/js/data/Use_Rest_js/Reservation_Rest.js
@@ -17,5 +17,50 @@ function getMovieCategory_List(e){
     let dataset=$(e.target).data("set");
 
 
+ $(e.currentTarget).find("a").removeClass("");
+    $(e.target).addClass("");
 
+    if(dataset.movie_category_lv||dataset.movie_category_lv){
+        console.log("1lv");
+        $().loading();
+        $(e.currentTarget).movie_category_remove();
+        movie_category_create();
+
+        $.ajax({
+            type:"get",
+            url:"/getMovieCategoryChild" +dataset.group_id + "/"+ dataset.movie_category_lv +"/"+ dataset.movie_category_detail_lv,
+               success: function(data){
+                console.log(data);
+                movie_category_create(data);
+
+               },
+               error: function(err, status){
+                console.log(err,status);
+               }
+        });
+
+
+        $(e.target).movie_category_set();
+
+    }
+
+}
+$.fn.movie_category_set= function(){
+    let movie_category_id= this.data("set").movie_category_id;
+    let group_id= this.data("set").group_id;
+    $("")
+}
+$.fn.movie_category_remove=function (){
+    while(this.next().length !=0){
+        $(this).next().remove();
+    }
+
+    function movie_category_create(data){
+        console.log(data);
+        if(!Array.isArray(data)){
+            console.log("data is not an array or is undefine:", data);
+            return;
+        }
+
+    }
 }
