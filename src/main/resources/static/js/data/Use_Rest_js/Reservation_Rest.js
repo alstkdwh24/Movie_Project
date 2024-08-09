47592e387e9e51743abadb084156bbfa72f74c16
alstkdwh24 <alstkdwh24@naver.com>
2024-08-09 18:26:57 +0900

    언녕

--- src/main/resources/static/js/data/Use_Rest_js/Reservation_Rest.js
+++ src/main/resources/static/js/data/Use_Rest_js/Reservation_Rest.js
@@ -15,12 +15,12 @@ $.ajax({
 });
 
 $(document).ready(function () {
-    $('.sub_menu_toggle > a').click(function () {
+    $('.sub_menu_toggle > div').click(function () {
         if (!$(this).hasClass("sub_menu_select")) {
-            $(this).next('ul').slideDown();
+            $(this).next('div').slideDown();
             $(this).addClass('sub_menu_select');
         } else {
-            $(this).next('ul').slideUp();
+            $(this).next('div').slideUp();
             $(this).removeClass("sub_menu_select");
         }
     });
@@ -29,12 +29,12 @@ $(document).ready(function () {
 function getMovieCategory_List(e) {
 
     e.preventDefault();
-    if (e.target.tagName !== 'A')
-        return;
+    // if (e.target.tagName !== 'div')
+    //     return;
 
     let set = $(e.target).data("set");
 
-    $(e.currentTarget).find("a").removeClass("sub_menu_select");
+    $(e.currentTarget).find("div").removeClass("sub_menu_select");
     $(e.target).addClass("sub_menu_select");
 
     if (set.hasOwnProperty('movie_category_lv')) {
@@ -102,6 +102,37 @@ $.fn.movie_category_remove = function () {
         $(this).next().remove();
     }
 };
+function movie_category_createtwo(data) {
+    console.log(data);
+    if (!Array.isArray(data)) {
+        console.log("data is not an array or is undefined:", data);
+        return;
+    }
+    console.log("data is an array or is defined:", data);
+    let movie_category = '<div class="movies_titles" onclick="getMovieCategory_List(event);">';
+    data.forEach(function (result, index) {
+        movie_category += '<div class="movie_name" data-set=\'' + JSON.stringify(result) + '\'  >' + result.movie_detail_title + '<input type="hidden" name="movie_title" th:value="result.movie_detail_title"></div>';
+    });
+
+    movie_category += '</div>';
+    $("#reservation_board").append(movie_category);
+}
+
+function movie_category_create(data) {
+    console.log(data);
+    if (!Array.isArray(data)) {
+        console.log("data is not an array or is undefined:", data);
+        return;
+    }
+    console.log("data is an array or is defined:", data);
+    let movie_category = '<div class="movies_titles" onclick="getMovieCategory_List(event);">';
+    data.forEach(function (result, index) {
+        movie_category += '<div href="#" class="movie_name" data-set=\'' + JSON.stringify(result) + '\'  >' + result.movie_detail_title + '<input type="hidden" name="movie_place" th:value="result.movie_detail_title"></div>';
+    });
+
+    movie_category += '</div>';
+    $("#reservation_board").append(movie_category);
+}
 
 $.fn.loading = function () {
     $(".loading").css({display: "block"});
@@ -110,47 +141,27 @@ $.fn.loading = function () {
     }, 1000);
 };
 
+//---------------------------------------------------------------------------------------------------------------------------------------
 
-function movie_category_create(data){
-    createMovieCategory(data,'movie_place');
-}
-
-
-function movie_category_createtwo(data){
-    createMovieCategory(data,'movie_title');
-}
-
-
-function movie_category_creates(data){
-    createMovieCategory(data,'movie_time');
-}
-
-function createMovieCategory(data, clickHandler){
+function movie_category_creates(data) {
     console.log(data);
-    if(!Array.isArray(data)){
-        console.log("data is not an array or is undefined", data);
+    if (!Array.isArray(data)) {
+        console.log("data is not an array or is undefined:", data);
         return;
     }
+    console.log("data is an array or is defined:", data);
+    let movie_category = '<div class="movies_titles" onclick="Movie_reservation_modal(event);">';
+    data.forEach(function (result, index) {
+        movie_category += '<div class="movie_name" data-set=\'' + JSON.stringify(result) + '\'   ' +' data-id="result.movie_detail_title" th:value="${result.movie_detail_title}">' + result.movie_detail_title  + ' <input type="hidden" name="movie_time" id="movie_time" th:value="${result.movie_detail_title}"></div>';
 
-    let movie_category = '<ul class="movies_titles" onclick="getMovieCategory_List(event);">';
-    data.forEach(function(result, index){
-        let onClickFunction = index === data.length -1
-            ? 'Movie_reservation_modal(event)'
-            : '${clickHandler}(event)';
 
-        movie_category += `
-            <li class="movie_name">
-                <a href="#" data-set='${JSON.stringify(result)}' id="movie_title" onclick="${onClickFunction}">
-                    ${result.movie_detail_title}
-                </a>
-            </li>`;
-
+        console.log(movie_category.value);
     });
 
-    movie_category +='</ul>';
+
+    movie_category += '</div>';
     $("#reservation_board").append(movie_category);
 }
-//---------------------------------------------------------------------------------------------------------------------------------------
 
 let reservation_container=document.getElementById("reservation_modal");
 let closecl=document.getElementById("closecl");
@@ -158,16 +169,16 @@ let reservation_modal_contents=document.getElementById("reservation_modal_conten
 
 let reservation_submit=document.querySelectorAll("#reservation_submit");
 reservation_submit.onclick=function (){
-    document.reservation.action="reservation_resist";
-    document.reservation.submit();
-    document.reservation.method="Post"
-    console.log("안녕");
+
 }
 
 function Movie_reservation_modal() {
 
     reservation_container.style.display="flex";
-
+    document.reservation.action="reservation_resist";
+    document.reservation.submit();
+    document.reservation.method="Post"
+    console.log("안녕");
 }
 closecl.onclick=function (){
 
@@ -194,3 +205,5 @@ reservation_container.onclick=function (e){
 //         alert('카테고리를 불러오는데 실패하였습니다. F5를 눌러서 새로고침을 해주세요');
 //     }
 // });
+
+
