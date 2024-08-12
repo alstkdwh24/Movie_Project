2615a13b3d315318780810ee4147457197016ed5
alstkdwh24 <alstkdwh24@naver.com>
2024-08-12 23:39:20 +0900

    s

--- src/main/java/com/example/movie/controller/RestReservation.java
+++ src/main/java/com/example/movie/controller/RestReservation.java
@@ -1,10 +1,14 @@
 package com.example.movie.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RestReservation {
 
-
+    @GetMapping("/Reservation_Seat")
+    public String Reservation_Seat(){
+return null;
+    }
 
 }
