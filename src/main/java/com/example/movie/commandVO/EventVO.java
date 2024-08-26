39eda79fefb09153e9a5b073deb671aa4c00477a
alstkdwh24 <alstkdwh24@naver.com>
2024-08-26 14:26:54 +0900

    ss

--- src/main/java/com/example/movie/commandVO/EventVO.java
+++ src/main/java/com/example/movie/commandVO/EventVO.java
@@ -10,6 +10,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class EventVO {
+    private String username;
+
     private Integer free_number;
     private Integer free_like;
     private String free_contents;
