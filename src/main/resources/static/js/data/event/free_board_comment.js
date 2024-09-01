b89ec4e22a68081fbf57794377d38753678c67b5
alstkdwh24 <alstkdwh24@naver.com>
2024-09-01 21:20:59 +0900

    first

--- src/main/resources/static/js/data/event/free_board_comment.js
+++ src/main/resources/static/js/data/event/free_board_comment.js
@@ -7,7 +7,7 @@ function free_comment() {
         '    <div id="talk_contents_title"><div id="id"><div id="ids">아이디</div><div id="username"></div></div> <div id="write_time"><div id="write">작성시간</div><div id="comment_date"></div></div></div>' +
         '    <div id="talk_contents_content"><label id="label">' +
         '        <textarea id="textarea"></textarea>' +
-       ' <button id="talk_button_free">댓글</button>'+
+       ' <button id="talk_button_free">댓글등록</button>'+
         '    </div></label>' +
 
         '</div>' +
@@ -18,8 +18,8 @@ function free_comment() {
 }
 
 let textarea = document.getElementById("textarea");
-let write_time = document.getElementById("write_time");
-
+let write_time = document.getElementById("write");
+let username=document.getElementById("username");
 // 함수 호출
 free_comment();
 
