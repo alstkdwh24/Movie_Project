f5d8b263b9deb4df54fae5c6d142304fe97ac82d
alstkdwh24 <alstkdwh24@naver.com>
2024-10-15 00:29:44 +0900

    ss

---
+++ src/main/java/com/example/movie/commandVO/MainsVO/EventVO_Board.java
@@ -0,0 +1,21 @@
+package com.example.movie.commandVO.MainsVO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class EventVO_Board {
+    private Integer movie_event_image_number;
+    private String event_name;
+    private String movie_filename;
+    private String uuid;
+    private LocalDateTime reg_date;
+    private String movie_filepath;
+}
---
+++ src/main/java/com/example/movie/commandVO/MainsVO/MovieVO.java
@@ -0,0 +1,21 @@
+package com.example.movie.commandVO.MainsVO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class MovieVO {
+    private Integer movie_integer;
+    private String movie_title;
+    private String movie_filename;
+    private String uuid;
+    private LocalDateTime reg_date;
+    private String filepath;
+}
