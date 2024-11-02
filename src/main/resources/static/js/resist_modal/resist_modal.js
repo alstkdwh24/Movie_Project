a80645866333919d3a225a4c75384a9231956b05
alstkdwh24 <alstkdwh24@naver.com>
2024-11-02 22:18:54 +0900

    s

--- src/main/resources/static/js/resist_modal/resist_modal.js
+++ src/main/resources/static/js/resist_modal/resist_modal.js
@@ -1,7 +1,17 @@
-
-    function movie_modal_resist(event) {
-        let resist_modal = document.querySelector(".resist_black");
+let resist_modal = document.querySelector(".resist_black");
+let resist_modal_top=document.getElementById("resist_modal_top")
+function movie_modal_resist(event) {
         resist_modal.style.display = "flex"; // 선택한 요소의 display 속성을 'flex'로 설정하여 보이게 합니다.
         console.log("나옵니다. 모달만 만들면 되요.")
 
+
     }
+    resist_modal_top.onclick = function(event){
+        resist_modal.style.display="none";
+    }
+
+resist_modal.onclick = function(event){
+    if(resist_modal && event.target === resist_modal ){
+    resist_modal.style.display="none";}
+
+}
