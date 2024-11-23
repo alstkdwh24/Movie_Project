3ac66917987272eed9a429b295a764876a9ce514
alstkdwh24 <alstkdwh24@naver.com>
2024-11-24 00:23:22 +0900

    s

--- src/main/java/com/example/movie/controller/MovieController.java
+++ src/main/java/com/example/movie/controller/MovieController.java
@@ -38,20 +38,18 @@ public class MovieController  {
         UserDetails userDetails = (UserDetails) session.getAttribute("user");
         model.addAttribute("userSession", userDetails);
         ArrayList<MovieVO> movie_list=movie_image_service.movie_resist_list(vo);
-        String movieHtml=movie_resist_html(movie_list);
+        String movieHtml=movieResistHtml(movie_list);
         System.out.println("movie_list" + movie_list);
         model2.addAttribute("movie_list",movie_list);
         model2.addAttribute("movieHtml",movieHtml);
         return "movie/mains";
     }
-    private String movie_resist_html(ArrayList<MovieVO> movie_list){
-        StringBuilder html=new StringBuilder();
-        for(MovieVO movieVO: movie_list){
-            String imageUrl=movieVO.getMovie_resist_filePath();
-            html.append("<img src='").append(imageUrl).append("' alt='영화 이미지' />");
-
+    private String movieResistHtml(ArrayList<MovieVO> movieList) {
+        StringBuilder html = new StringBuilder();
+        for (int i = 0; i < movieList.size(); i++) {
+            String imageUrl = movieList.get(i).getMovie_resist_filePath();
+            html.append(" <div class=\"movie_image\">").append(" <div class=\"movie_real_image\">").append("<img src='").append(imageUrl).append("' alt='영화 이미지' />").append("</div>").append("</div>").append("  <div class=\"movie_gle\">[[${vo.movie_title}]]</div>");
         }
         return html.toString();
-
     }
 }
