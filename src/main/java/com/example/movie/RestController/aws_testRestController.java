895fd74ca854bd4310cdda1adfa9968e1612396a
alstkdwh24 <alstkdwh24@naver.com>
2025-01-21 01:14:57 +0900

    s

---
+++ src/main/java/com/example/movie/RestController/aws_testRestController.java
@@ -0,0 +1,23 @@
+package com.example.movie.RestController;
+
+import com.example.movie.ReservationService.ReservationService;
+
+import com.example.movie.commandVO.EventVO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+@RequiredArgsConstructor
+@RestController
+public class aws_testRestController {
+
+
+}
